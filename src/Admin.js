496f65d34e75f1af3ec95ed358b5d7b2abd91166
Anmol <88301047+The-Anmol@users.noreply.github.com>
2022-07-22 23:03:20 +0530

    add: working of filter toggle

--- src/Admin.js
+++ src/Admin.js
@@ -33,59 +33,8 @@ const Admin = () => {
                 <img  src="./Asset/Icon/filter.svg" height={25} width={25} alt="" />Filter
                </button>
                <div className='grid grid-cols-1 justify-between xl:grid-cols-2'>
-                <div className={filterToggled ? "show" : " "} >
-                  <div className='filter-institution hidden w-fit text-center justify-center mb-4'>
-                      <div className='text-lg' >Educational Institutions</div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Girls School – Sirsa ( Haryana )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Girls College – Sirsa ( Haryana )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Boys School – Sirsa ( Haryana )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Boys College – Sirsa ( Haryana )</label>
-                      </div>                 
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji College of Education – Sirsa ( Haryana )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Saint MSG Glorious International School – Sirsa ( Haryana )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Noble School – Kotda, Udaipur ( Rajasthan )</label>
-                      </div>                  
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Girls School – Tara Nagar, Churu ( Rajasthan )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Girls School – Budhni, Sehore ( Madhya Pradesh )</label>
-                      </div> 
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Girls School – Sri Gurusar Modia, Sri Ganganagar ( Rajasthan )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Girls College – Sri Gurusar Modia, Sri Ganganagar ( Rajasthan )</label>
-                      </div>
-                      <div className="flex items-center space-x-2 py-2">
-                          <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
-                          <label className="text-sm font-semibold text-gray-500">Shah Satnam Ji Boys School – Sri Gurusar Modia, Sri Ganganagar ( Rajasthan )</label>
-                      </div>
-                  </div>
-                  <div className='filter-institution hidden w-fit text-center justify-center mb-4'>
+                <div className={filterToggled ? "hide" : "show"} >
+                  <div className=' w-ful text-center justify-center mb-4'>
                       <div className='text-lg' >Educational Institutions</div>
                       <div className="flex items-center space-x-2 py-2">
                           <input type="checkbox" className="w-4 h-4 transition duration-300 rounded focus:ring-2 accent-blue-500 focus:ring-offset-0 focus:outline-none focus:ring-blue-200" />
